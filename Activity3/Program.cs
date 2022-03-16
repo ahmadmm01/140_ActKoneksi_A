a725988e8ecf8f844fe2b1b3da5ccc88ea052e25
ahmadmm01 <skyahmad74@gmail.com>
2022-03-16 14:39:27 +0700

    Initial commit

---
+++ Activity3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Activity3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Connecting();
+        }
+
+        public void Connecting() 
+        {
+            using (SqlConnection con = new SqlConnection("data source= MADD\\AHMADMM; database=ProdiTI; User ID= sa; Password=root"))
+            {
+                con.Open();
+                Console.WriteLine("Berhasil Terhubung ke Database");
+            }
+        }
+    }
+}
